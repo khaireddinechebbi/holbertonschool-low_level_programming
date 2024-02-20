51157c435c8ee59874cec726717de43673902fe7
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-20 08:49:04 +0000

    6-size

---
+++ hello_world/6-size.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: 'prints the size of various types of char'
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+printf("Size of a char: %lu byte(s)\n", sizeof(char));
+return (0);
+}
