0893404ad3aecd636b406e0a8969f7c2d0e0af72
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-20 19:40:25 +0100

    101 quote

---
+++ hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'the program prints quote'
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n")
+return (1);
+}
