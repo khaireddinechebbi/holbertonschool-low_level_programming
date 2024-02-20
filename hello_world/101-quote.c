9648ba68645cc4320233c593b3f386bf862dc895
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-20 19:53:13 +0100

    101 quote

--- hello_world/101-quote.c
+++ hello_world/101-quote.c
@@ -7,8 +7,6 @@
  */
 int main(void)
 {
-    /* Using the write function to print to standard error */
-    /* File descriptor for standard error: 2 */
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
