1037b94626f57e647bab0df1aac0e5d2c8ca3529
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-21 12:54:04 +0100

    3 print alphabets

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - print if number is positive
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char i, j;
+for (i = 'a'; i <= 'z'; i++)
+{
+putchar(i);
+}
+for (j = 'A'; j <= 'Z'; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
