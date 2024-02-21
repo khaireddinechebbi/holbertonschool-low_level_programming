6e55178c964ed9b28ad5a18299a7068a1874e5a8
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-21 22:17:45 +0100

    102 print multiple combinations

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -5,29 +5,20 @@
 */
 int main(void)
 {
-int i, j, x, y;
-for (i = 0; i < 10; i++)
+int i, j;
+for (i = 0; i < 100; i++)
 {
-for (j = 0; j < 10; j++)
+for (j = i + 1; j < 100; j++)
 {
-for (x = i; x < 10; x++)
-{
-for (y = j; y < 10; y++)
-{
-if (i != x || j != y)
-{
-putchar(i + '0');
-putchar(j + '0');
+putchar(i / 10 + '0');
+putchar(i % 10 + '0');
 putchar(' ');
-putchar(x + '0');
-putchar(y + '0');
-if (i != 9 || j != 8 || x != 9 || y != 9)
+putchar(j / 10 + '0');
+putchar(j % 10 + '0');
+if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
-}
-}
-}
 }
 }
 }
