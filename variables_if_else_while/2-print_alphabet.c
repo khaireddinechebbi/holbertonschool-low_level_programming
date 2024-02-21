c8179a39520423f80a756bb35eed59c7f69e7ce2
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-21 12:38:53 +0100

    2 print alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print if number is positive
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+char str[100] = "abcdefghijklmnopqrst";
+for (int i = 0; i < strlen(str); i++)
+{
+putchar(str[i]);
+}
+}
