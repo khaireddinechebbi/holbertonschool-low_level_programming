2b5373350dd76b5aa6472ff167ca96e01d210a4b
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-21 11:16:57 +0100

    1 last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -18,13 +18,13 @@ printf("Last digit of %d is %d and is greater than 5 \n", n, r);
 }
 else
 {
-if (r < 6)
+if (r == 0)
 {
-printf("Last digit of %d is %d and is less than 6 and not 0 \n", n, r);
+printf("Last digit of %d is %d and is 0 \n", n, r);
 }
 else
 {
-printf("Last digit of %d is %d and is 0", n, r);
+printf("Last digit of %d is %d and is less than 6 and not 0", n, r);
 }
 }
 return (0);
