c2509faf989608f34e55290e9e1f58fb1a9822a1
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-21 22:01:51 +0100

    102 print multiple combinations

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - print if number is positive
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i, j, x, y;
+for (i = 0; i < 10; i++)
+{
+for (j = 0; j < 10; j++)
+{
+for (x = i; x < 10; x++)
+{
+for (y = j; y < 10; y ++)
+{
+if (i != x || j != y)
+{
+putchar(i + '0');
+putchar(j + '0');
+putchar(' ');
+putchar(x + '0');
+putchar(y + '0');
+if (i !=9 || j != 8 || x != 9 || y != 9)
+{
+putchar(',');
+}
+}
+}
+}
+}
+}
+putchar('\n');
+return (0);
+}
