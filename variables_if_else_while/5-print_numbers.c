2426670c083ec6678ddfb92cf732826b2c7ed34e
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-21 13:32:55 +0100

    5 print numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - print if number is positive
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+for (i = 0; i <= 10; i++)
+{
+printf("%d\n", i);
+}
+return (0);
+}
