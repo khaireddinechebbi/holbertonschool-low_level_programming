f04d2b6308a4b0f0f3e4b5fa5c5708cd14e1314b
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-21 14:43:02 +0100

    8 print base 16 numbers

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - print if number is positive
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+int i;
+char j;
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+for (j = 'a'; j <= 'f'; j++)
+{
+putchar(j);
+}
+putchar('\n');
+return (0);
+}
