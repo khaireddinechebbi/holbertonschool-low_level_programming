5165211f80a88e95f0fb29cb7a7699e6e467f7d6
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-21 22:21:50 +0100

    100 print double combination

--- variables_if_else_while/100-print_comb3.c
+++ variables_if_else_while/100-print_comb3.c
@@ -8,9 +8,7 @@ int main(void)
 int i, j;
 for (i = 0; i < 9; i++)
 {
-for (j = i; j < 10; j++)
-{
-if (i != j)
+for (j = i + 1; j < 10; j++)
 {
 putchar(i + '0');
 putchar(j + '0');
@@ -18,7 +16,7 @@ if (i != 8 || j != 9)
 {
 putchar(',');
 putchar(' ');
-}
+
 }
 }
 }
