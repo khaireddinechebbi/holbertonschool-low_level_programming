4e24f74e383002ad26abf121beeb739207170f23
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-22 17:45:42 +0100

    4 is alpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isalpha - print if number is positive
+ * Description: 'alphabet'
+ * @c: a character
+ * Return: 1 if lowercase, otherwise 0
+*/
+int _isalpha(int c)
+{
+if (c >= 'A' && c <= 'z')
+{
+if (c <= 'Z' && c >= 'a')
+{
+return (1);
+}
+}
+else
+{
+return (0);
+}
+}
