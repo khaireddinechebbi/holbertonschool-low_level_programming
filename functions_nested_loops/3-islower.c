4fdd044689409490a37dc8246159c14604ded1e5
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-22 14:02:26 +0100

    3 is lowercase

---
+++ functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - print if number is positive
+ * Description: 'alphabet'
+*/
+int _islower(int c)
+{
+if (c >= 'a' && c <= 'z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
