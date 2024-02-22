579e8584975eb31daf8011e990b56dba2d670c2e
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-22 12:16:50 +0100

    1 alphabet

--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -3,12 +3,9 @@
  * main - print if number is positive
  * Description: 'alphabet'
 */
+void print_alphabet(void);
 int main(void)
 {
-char i;
-for (i = 'a'; i <= 'z'; i++)
-{
-_putchar(i);
-}
-_putchar('\n');
+print_alphabet();
+return (0);
 }
