8828a7aa254ec9de28d506212e0137c4eb5f623c
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-23 13:19:41 +0100

    8 time

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* jack_bauer - prints every minutes
+* @
+*/
+void jack_bauer(void)
+{
+int mn, hr ;
+for (hr = 0; hr < 24; hr++)
+{
+for (mn = 0; mn < 60; mn++)
+{
+_putchar(hr/10 + '0');
+_putchar(hr%10 + '0');
+_putchar(':');
+_putchar(mn/10 + '0');
+_putchar(mn%10 + '0');
+}
+}
+}
