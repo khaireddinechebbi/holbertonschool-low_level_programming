b01008f7b8d9af39950fe7693b6a4dcdc68e3388
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-23 14:15:23 +0100

    9 times tables

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -10,37 +10,28 @@ for (i = 0; i < 10; i++)
 for (j = 0; j < 10; j++)
 {
 k = i * j;
+if (j != 0)
+{
+_putchar(' ');
+}
+if (k < 10)
+{
+if (j != 0)
+{
+_putchar(' ');
+}
+_putchar(k + '0');
+}
+else
+{
+_putchar(k / 10 + '0');
+_putchar(k % 10 + '0');
+}
 if (j != 9)
 {
-if (k < 10)
-{
-_putchar(k + '0');
 _putchar(',');
-_putchar(' ');
-_putchar(' ');
-}
-else
-{
-_putchar(k / 10 + '0');
-_putchar(k % 10 + '0');
-_putchar(',');
-_putchar(' ');
 }
 }
-else
-{
-if (k < 10)
-{
-_putchar(k + '0');
 _putchar('\n');
-}
-else
-{
-_putchar(k / 10 + '0');
-_putchar(k % 10 + '0');
-_putchar('\n');
-}
-}
-}
 }
 }
