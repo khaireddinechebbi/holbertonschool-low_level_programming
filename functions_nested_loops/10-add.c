a35319e6cb7da0f67e1a53eea471d17d3146a813
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-24 22:36:59 +0100

    10 add

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -9,14 +9,51 @@ int add(int a, int b)
 {
 int i;
 i = a + b;
-if (i < 10)
+
+if (i < 10 && i > -10)
+{
+if (i < 0)
+{
+i *= -1;
+_putchar('-');
+_putchar(i + '0');
+}
+else
 {
 _putchar(i + '0');
+}
+}
+else if (i < 100 && i > -100)
+{
+if (i < 0)
+{
+i *= -1;
+_putchar('-');
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
 }
 else
 {
 _putchar(i / 10 + '0');
 _putchar(i % 10 + '0');
+}
+}
+else
+{
+if (i < 0)
+{
+i *= -1;
+_putchar('-');
+_putchar(i / 100 + '0');
+_putchar((i % 100) / 10 + '0');
+_putchar(i % 10 + '0');
+}
+else
+{
+_putchar(i / 100 + '0');
+_putchar((i % 100) / 10 + '0');
+_putchar(i % 10 + '0');
+}
 }
 _putchar('\n');
 return (i);
