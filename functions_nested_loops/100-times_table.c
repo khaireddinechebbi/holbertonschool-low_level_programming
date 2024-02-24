f042d01f311d748c59c1bf5411b65ca963051868
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-24 23:17:46 +0100

    100 times table

---
+++ functions_nested_loops/100-times_table.c
@@ -0,0 +1,40 @@
+#include "main.h"
+/**
+* print_times_table - print the 9 times table
+*/
+void print_times_table(int n)
+{
+int i, j, k;
+if (n >= 0 && n <= 15)
+{
+for (i = 0; i <= n; i++)
+{
+_putchar('0');
+for (j = 1; j <= 15; j++)
+{
+k = i * j;
+_putchar(',');
+_putchar(' ');
+if (k < 10)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(k + '0');
+}
+else if (k < 100)
+{
+_putchar(' ');  
+_putchar(k / 10 + '0');
+_putchar(k % 10 + '0');
+}
+else
+{
+_putchar(k / 100 + '0');
+_putchar((k % 100) / 10 + '0');
+_putchar(k % 10 + '0');
+}
+}
+_putchar('\n');
+}
+}
+}
