d44f047b576c5923e6f7675c28130691a7076da7
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-24 23:55:36 +0100

    102 fibonacci numbers

---
+++ functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+* main - print first 50 fibonacci numbers
+* Return: 0 successful
+*/
+int main(void)
+{
+int i = 0;
+int j = 1;
+int k = 2;
+while (i < 50)
+{
+if (i == 0)
+{
+printf("%d", j);
+}
+else if (i == 1)
+{
+printf(", %d", k);
+}
+else
+{
+k += j;
+j = k - j;
+printf(", %d", k);
+}
+}
+return (0);
+}
