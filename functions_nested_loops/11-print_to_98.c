bb487687e166f232fb2f98515aa48e26ff9fb602
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-24 10:37:34 +0100

    11 98

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -5,40 +5,40 @@
 */
 void print_to_98(int n)
 {
-    int i;
-    if (n < 98)
-    {
+int i;
+if (n < 98)
+{
 for (i = n; i <= 98; i++)
 {
-    if (i > -10 && i < 10)
-    {
-        _putchar(i + '0');
-        _putchar(',');
-        _putchar(' ');
-    }
-    else
-    {
-        _putchar(i / 10 + '0');
-        _putchar(i % 10 + '0');
-        if (i != 98)
-        {
-        _putchar(',');
-        _putchar(' ');
-        }
-    }
+if (i > -10 && i < 10)
+{
+_putchar(i + '0');
+_putchar(',');
+_putchar(' ');
 }
-    }
-    else
-    {
-        for (i = n; i >= 98; i--)
-        {
-        _putchar(i / 10 + '0');
-        _putchar(i % 10 + '0');
-        if (i != 98)
-        {
-        _putchar(',');
-        _putchar(' ');
-        }
-        }
-    }
+else
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
+}
+else
+{
+for (i = n; i >= 98; i--)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+if (i != 98)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+}
 }
