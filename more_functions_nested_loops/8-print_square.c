90600848e34b60fb1b94ec890b8851ef884c4f15
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-26 13:38:57 +0100

    8 suares

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -8,10 +8,12 @@ void print_square(int size)
 int l, c;
 if (size > 1)
 {
-for (l = 2; l <= size; l++)
+for (l = 1; l <= size; l++)
 {
-for (c = 2; c <= size; c++)
+for (c = 1; c <= size; c++)
+{
 _putchar('#');
+}
 _putchar('\n');
 }
 }
