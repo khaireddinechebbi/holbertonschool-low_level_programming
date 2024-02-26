c3ae0aa39710c76ecf53a6c9aa29882d002b72ed
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-26 23:51:32 +0100

    101 print numbers

--- more_functions_nested_loops/101-print_number.c
+++ more_functions_nested_loops/101-print_number.c
@@ -11,14 +11,16 @@ if (n < 0)
 n *= -1;
 _putchar('-');
 }
-while (n / i > 0)
+while (n / i > 9)
 {
 i *= 10;
 }
 while (i >= 10)
 {
+if (n != 0)
+_putchar(n / i + '0');
 n = n % i;
 i /= 10;
-_putchar(n / i + '0');
 }
+_putchar(n % 10 + '0');
 }
