63885f100e6d6e6b5bc40c41bdc65eb94e67e9f5
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-26 14:50:18 +0100

    9 fizz buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 int i;
-for (i = 1; i <= 100; i++)
+for (i = 1; i < 100; i++)
 {
 if (i % 3 == 0 && i % 5 != 0)
 printf("Fizz ");
@@ -17,5 +17,6 @@ printf("FizzBuzz ");
 else
 printf("%d ", i);
 }
+printf('Buzz\n');
 return (0);
 }
