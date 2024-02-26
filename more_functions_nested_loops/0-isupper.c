8beb494c99f7f94bde9170e9d61421dc75537e41
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-26 10:19:31 +0100

    0 is uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+* _isupper - checks for uppercases
+* Return: 1 if upper, otherwise 0
+* @c: character
+*/
+int _isupper(int c)
+{
+if (c >= 'A' && c <= 'Z')
+{
+return (1);
+}
+else
+{
+return (0);
+}
+}
