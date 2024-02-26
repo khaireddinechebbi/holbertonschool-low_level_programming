5c5bde64efae45034237a0ff4e8e2cbd5338cebc
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-26 12:01:26 +0100

    6 lines

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+* print_line - print line
+* @n: integer
+*/
+void print_line(int n)
+{
+int i;
+if (n != 0)
+for (i = 1; i <= n; i ++)
+{
+_putchar('_');
+}
+_putchar('\n');
+}
