e2291fda3b638a4f9d05763562e4c35c5c7e19bd
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-28 13:39:24 +0100

    4 print verse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,15 +6,14 @@
 void print_rev(char *s)
 {
 
-int i, ln;
-int l = 0;
-while (*s != '\0')
+int i = 0;
+while (s[i] != '\0')
 {
-l++;
-s++;
+i++;
 }
-ln = l;
-for (i = ln - 1; i >= 0; i--)
+for (i -= 1; i >= 0; i--)
+{
 _putchar(s[i]);
+}
 _putchar('\n');
 }
