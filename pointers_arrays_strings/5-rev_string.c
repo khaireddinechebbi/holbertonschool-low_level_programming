65c1cfef296e0082b73a30c3ce9c07aba93b3b24
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-28 14:16:24 +0100

    5 reverse string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+* rev_string - print verse string
+* @s: sting type char
+*/
+void rev_string(char *s)
+{
+int i = 0, ln, r;
+while (s[i] != '\0')
+{
+i++;
+}
+ln = i;
+for (i -= 1; i > ln / 2; i--)
+{
+r = s[i];
+s[i] = s[ln - i - 1];
+s[ln - i - 1] = r;
+}
+}
