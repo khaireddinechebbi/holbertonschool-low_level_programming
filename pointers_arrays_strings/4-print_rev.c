21a550771f4da040b85127aa17f8f6544067483c
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-28 12:58:58 +0100

    4 print verse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
 * rint_rev - print verse string
 * @s: sting type char
@@ -7,7 +6,7 @@
 void print_rev(char *s)
 {
 int i;
-for (i = strlen(s); i >= 0; i--)
+for (i = _strlen(s); i >= 0; i--)
 _putchar(s[i]);
 _putchar('\n');
 }
