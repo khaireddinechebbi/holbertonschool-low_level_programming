40592c4cba6d5a5cfa0425a79662c5f451903571
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-28 14:33:55 +0100

    6 puts

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,10 +1,15 @@
 #include "main.h"
 /**
+* puts2 - prints half the characters of string
 */
 void puts2(char *str)
 {
 int i;
-for (i = 0; str[i] != '\0'; i += 2)
+for (i = 0; str[i] != '\0'; i++)
+{
 _putchar(str[i]);
+i += 1;
+}
+
 _putchar('\n');
 }
