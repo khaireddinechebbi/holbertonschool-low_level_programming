c8600b3e02e52ba03ee31a0a5b2f5c766adf24a0
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-29 13:51:04 +0100

    100 atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,14 +1,20 @@
 #include "main.h"
 /**
+* _atoi - print numbers only
+* @s: string
 */
 int _atoi(char *s)
 {
-int i;
+int i, j = 0;
 for (i = 0; s[i] != ';'; i++)
 {
-if (s[i] <= 9 && s[i] >= 0)
 if (s[i] == '-')
-s[i] *= -1;
+{
+j += 1;
+}
+if (j % 2 != 0)
+_putchar('-');
+if (s[i] <= 57 && s[i] >= 48)
 _putchar(s[i] + '0');
 }
 return (0);
