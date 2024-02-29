78761dc7169b66c9614efd252c3921f3522df224
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-29 11:49:38 +0100

    9 strcpy

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* *_strcpy - copy string
+* @dest: pointer
+* @src: pointer
+* Return: NULL
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while ((dest[i] = src[i]) != '\0')
+{
+i++;
+}
+return ('\0');
+}
