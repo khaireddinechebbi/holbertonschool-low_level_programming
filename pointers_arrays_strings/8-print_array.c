05fd3a7b177d4000323f277cb1b4e2aa9f37ecda
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-02-29 11:07:24 +0100

    8 print array

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -8,8 +8,11 @@ void print_array(int *a, int n)
 {
 int i;
 if (n > 0)
+{
 for (i = 0; i < n; i++)
 if (i != n - 1)
 printf("%d, ", a[i]);
-printf("%d\n", a[n - 1]);
+printf("%d", a[n - 1]);
+}
+printf("\n");
 }
