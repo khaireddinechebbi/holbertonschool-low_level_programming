5cea209ff1402f4ee12cc59272887f57f5abaacb
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-01 14:11:12 +0100

    1 strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+*/
+char *_strncat(char *dest, char *src, int n)
+{
+int i = 0, j;
+while (dest[i] != '\0')
+i++;
+for (j = 0; j <= n && src[j] != '\0'; j++, i++)
+{
+dest[i] = src[j];
+}
+return (dest);
+}
