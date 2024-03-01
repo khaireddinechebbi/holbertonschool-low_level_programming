2393a2c6801d164b4677e7d9ee6ae41fe5038e38
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-01 14:48:37 +0100

    2 strncpy

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* *_strncpy - copy strings
+* @dest: string
+* @src: string
+* @n: int
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i = 0;
+while ((dest[i] = src[i]) && i < n)
+i++;
+return (dest);
+}
