646ace76c7428fab135db91ce687343f590409c0
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-02 20:14:04 +0100

    100 rot13

---
+++ pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* *rot13 - change alphabet by numners
+* @s: string
+* Return: s successful
+*/
+char *rot13(char *s)
+{
+int i, j;
+char *alpha = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char *rt = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j = 0; alpha[j] != '\0'; j++)
+{
+if (s[i] == alpha[j])
+{
+s[i] = rt[j];
+}
+}
+}
+return (s);
+}
