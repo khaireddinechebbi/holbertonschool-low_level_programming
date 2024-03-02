0713edf166a117c9d2ae71354cf20f9a08ef52cd
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-02 16:00:24 +0100

    5 string toupper

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,16 @@
 #include "main.h"
 /**
+* *string_toupper - change lowercse to uppercase
+* @s: string
+* Return: s
 */
-char *string_toupper(char *)
+char *string_toupper(char *s)
 {
-int i, n;
-char s[n];
+int i = 0, n = 0;
+while (s[i])
+{
+n++;
+}
 for (i = 0; i < n; i++)
 {
 if (s[i] <= 'z' && s[i] >= 'a')
