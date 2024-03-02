1eb0b855d608807dd83eda7676fc6d23b2473410
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-02 13:46:20 +0100

    2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -4,12 +4,12 @@
 * @dest: string
 * @src: string
 * @n: int
+* Return: dest
 */
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
-while ((dest[i] = src[i]) && i < n)
+while ((dest[i] = src[i]) && i <= n)
 i++;
 return (dest);
-return (0);
 }
