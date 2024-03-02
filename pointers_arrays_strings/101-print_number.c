ef16b83c9dd8a0120bfcbfc74fc999c37c512f97
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-02 21:02:45 +0100

    101 print numbers

--- pointers_arrays_strings/101-print_number.c
+++ pointers_arrays_strings/101-print_number.c
@@ -1,21 +1,26 @@
 #include "main.h"
-#include <limits.h>
-
 /**
- * print_number - Prints an integer
- * @n: The integer to be printed
- */
+* print_number - print numbers
+* @n: integer
+*/
 void print_number(int n)
 {
+int i = 10;
 if (n < 0)
 {
-if (n != INT_MIN)
+n *= -1;
 _putchar('-');
-n = -n;
 }
-if (n / 10 != 0)
+while (n / i > 9)
 {
-print_number(n / 10);
+i *= 10;
 }
-_putchar((n % 10) + '0');
+if (n > 9)
+while (i >= 10)
+{
+_putchar(n / i + '0');
+n = n % i;
+i /= 10;
+}
+_putchar(n % 10 + '0');
 }
