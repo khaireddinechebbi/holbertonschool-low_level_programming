bbf5b288ec071a6140a26cf4aaf391edd089dc02
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-02 14:57:14 +0100

    2 strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,15 +9,13 @@
 char *_strncpy(char *dest, char *src, int n)
 {
 int i = 0;
-while (i < n)
+for (i = 0; i < n && src[i] != '\0'; i++)
 {
 dest[i] = src[i];
-if (src[i] == '\0')
-{
-i++;
-break;
 }
-i++;
+for (; i < n; i++)
+{
+dest[i] = '\0';
 }
 return (dest);
 }
