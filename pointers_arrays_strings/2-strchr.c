1dbcec08ebef0e3eb6866f2dd18014012d5afef4
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-04 11:48:14 +0100

    2 strchr

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+* *_strchr - locates a character in a string
+* @s: string
+* @c: character
+* Return: r
+*/
+char *_strchr(char *s, char c)
+{
+unsigned int i = 0;
+while (s[i] != '\0')
+{
+if (s[i] != c)
+i++;
+else
+break;
+}
+return (s + i);
+}
