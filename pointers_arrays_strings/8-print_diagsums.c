4336dd22ca9b37e21b3ae1d877ea86554e320491
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-05 13:34:37 +0100

    8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,11 +9,11 @@ void print_diagsums(int *a, int size)
 {
 int i, j;
 unsigned int l = 0, w = 0;
-for (i = 0; i < size**2; i += size + 1)
+for (i = 0; i < size * size; i += size + 1)
 {
 l += a[i];
 }
-for (j = size - 1; j < size**2 - 1; j += size - 1)
+for (j = size - 1; j < size * size - 1; j += size - 1)
 {
 w += a[i];
 }
