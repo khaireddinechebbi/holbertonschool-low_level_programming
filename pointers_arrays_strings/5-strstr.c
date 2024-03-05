3fa6b8f28a8c669f0fbf7798adf16b7b654e97e7
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-05 11:04:08 +0100

    5 strstr

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* *_strpbrk -  locates a substring
+* @haystack: string
+* @needle: string
+* Return: haystack
+*/
+char *_strstr(char *haystack, char *needle)
+{
+int i = 0;
+while (*haystack)
+{
+while (*haystack == needle[i])
+{
+return (haystack);
+i++;
+}
+haystack++;
+}
+return (0);
+}
