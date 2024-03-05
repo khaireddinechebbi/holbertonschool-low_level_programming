f861356a81a886dd8407a0dd22090dc7ce1bed78
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-05 13:05:00 +0100

    8 print diagrams

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* print_diagsums - print diagrams
+* @a: array square
+* @size: int
+*/
+void print_diagsums(int *a, int size)
+{
+int i, j;
+unsigned int l = 0, w = 0;
+for (i = 0; i < size; i++)
+{
+while (j == i)
+{
+l += a[i][i];
+}
+while (j == size - i - 1)
+{
+w += a[i][size - i -1];
+}
+}
+printf("%d, %d", l, w);
+}
