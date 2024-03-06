53b6a412de3fe1770f44d3210e17f4dba9379edd
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-06 12:36:50 +0100

    2 strlen

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+* _strlen_recursion - print string's length
+* @s: string
+*/
+int _strlen_recursion(char *s)
+{
+int n;
+if (*s != '\0')
+_strlen_recursion(s + 1)
+_putchar(n);
+}
