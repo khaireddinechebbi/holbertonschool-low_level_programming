793a06cd72f0617ca7f52c119cbf7a8f406a6659
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-07 12:54:42 +0100

    5 sqrt

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,24 +1,29 @@
 #include "main.h"
 /**
+*/
+int power_err(int x, int n)
+{
+if (x * x == n)
+return (x);
+else if (x * x > n)
+return (-1);
+else
+return (power_err(x + 1, n));
+
+}
+
+/**
 * _sqrt_recursion - print the sqrt of numbers
 * @n: int
 * Return: result
 */
 int _sqrt_recursion(int n)
 {
-int x = 0;
-if (n >= 0)
-{
-if (x * x != n && x * x < n)
-{
-x += 1;
-_sqrt_recursion(n);
-}
-if (x * x == n)
-return (x);
-if (x * x > n)
+int x = 1;
+if (n < 0)
 return (-1);
-}
-else if (n < 0)
-return (-1);
+else if (n == 0)
+return (0);
+else
+return (power_err(x, n));
 }
