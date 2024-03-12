b503acaeac2d4f4a6e221e23ec882fa42d835af5
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-12 10:26:42 +0100

    2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -11,8 +11,10 @@ char *str_concat(char *s1, char *s2)
 int i, j, l1 = 0, l2 = 0;
 char *s;
 
-if (s1 == NULL || s2 == NULL)
-return (NULL);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
 while (s1[l1])
 l1++;
