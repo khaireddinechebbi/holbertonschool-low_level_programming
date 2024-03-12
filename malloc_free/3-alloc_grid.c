60552697d9fdeddbed9797b646917075e89bb9eb
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-12 13:53:07 +0100

    3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -21,16 +21,19 @@ for (h = 0; h < height; h++)
 A[h] = malloc(sizeof(int) * width);
 if (A[h] == NULL)
 {
-return (NULL);
-free(A[h]);
+for (h--; h >= 0; h--)
+{
+    free(A[h]);
+    free(A);
+    return (NULL);
+}
+
 }
 for (w = 0; w < width; w++)
 {
 A[h][w] = 0;
-
 }
-free(A[h]);
 }
 return (A);
-free(A);
+
 }
