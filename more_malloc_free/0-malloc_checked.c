12a6dab761d6db85f6c613c09759cf55b3f15d2c
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-13 12:11:52 +0100

    0 malloc checked

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -8,9 +8,9 @@ void *malloc_checked(unsigned int b)
 {
 void *ptr = malloc(b);
 
-if (ptr == NULL) {
-fprintf(stderr, "Error: malloc failed\n");
-exit(98); // Terminate with status value 98
+if (ptr == NULL)
+{
+exit(98);
 }
     
 return ptr;
