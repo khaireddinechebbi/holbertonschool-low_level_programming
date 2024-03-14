d2c081e807552ca23218c78a6ac71cec2d2227ca
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-14 12:13:36 +0100

    1 tsring

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -1,33 +1,59 @@
-#include "main.h"
 #include <stdlib.h>
+#include "main.h"
 /**
+ * _strlen - function calcul string
+ * @s: string
+ * Return: length
+*/
+int _strlen(char *s)
+{
+int length = 0;
+while (s[length])
+{
+length++;
+}
+return (length);
+}
+
+/**
+ * *string_nconcat - function for concatenate two function
+ * @s1: string
+ * @s2: string
+ * @n: length
+ * Return: new string concatenate two string
 */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-int i, j, ln1 = 0, ln2 = 0;
 char *s;
-
+unsigned int length1, length2, i, j, length;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 s2 = "";
-
-while (s1[ln1])
-ln1++;
-while (s2[ln2])
-ln2++;
-
-if (ln2 > n)
+length1 = _strlen(s1);
+length2 = _strlen(s2);
+if (n >= (unsigned int)length2)
 {
-s = malloc(sizeof(char) * (k + n + 1));
-
+length = length1 + length2;
 }
-
-if (ln2 <= n)
-s = malloc(sizeof(char) * (k + j + 1));
-
+else
+length = length1 + n;
+s = malloc((length + 1) * sizeof(char));
 if (s == NULL)
+{
 return (NULL);
-
-for (i = 0; )
+}
+else
+{
+for (i = 0; i < length1; i++)
+{
+s[i] = s1[i];
+}
+for (j = 0; j < n && s2[j] != '\0'; j++)
+{
+s[i + j] = s2[j];
+}
+s[i + j] = '\0';
+return (s);
+}
 }
