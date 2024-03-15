e46188a36beebfcbe58eef012791790801337c39
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-15 10:37:21 +0100

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -14,8 +14,3 @@ char *owner;
 } my_dog;
 
 #endif
-
-void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
-void free_dog(dog_t *d);
