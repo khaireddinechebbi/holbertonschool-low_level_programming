b3a0d6703fe3c66090f187491d7113e7807f90e3
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-15 11:51:26 +0100

    dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,12 +1,12 @@
-#ifndef dog
-#define dog
+#ifndef DOG_H
+#define DOG_H
 /**
 * struct dog - struct
 * @name: char *
 * @age: float
 * @owner: char *
 */
-struct dog
+typedef struct dog
 {
 char *name;
 float age;
@@ -14,7 +14,8 @@ char *owner;
 } my_dog;
 
 /* Function prototype */
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
