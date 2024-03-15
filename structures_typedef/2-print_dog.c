8a6e22fbb47f7a700977346c10c9ebb663f7abb5
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-15 12:11:41 +0100

    2 print dog

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,7 +9,7 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
-d->mame ? printf("Name:%s\n", d->name) : printf("nil\n");
+d->name ? printf("Name:%s\n", d->name) : printf("nil\n");
 d->age ? printf("Name:%f\n", d->age) : printf("nil\n");
 d->owner ? printf("Name:%s\n", d->owner) : printf("nil\n");
 }
