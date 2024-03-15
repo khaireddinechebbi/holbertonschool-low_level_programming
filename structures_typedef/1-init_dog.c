eeaf25bb435e2e04237eba800cc98bba0eee5efe
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-15 10:49:32 +0100

    1 init dog

---
+++ structures_typedef/1-init_dog.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+#include "dog.h"
+
+/**
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+if (d != NULL)
+{
+d.name = name;
+d.age = age;
+d.owner = owner;
+}
+}
