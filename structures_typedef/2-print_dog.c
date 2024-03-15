44e41717d30606975e6a265f5eec14b8f8f5169e
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-15 12:09:41 +0100

    2 print dog

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,16 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+* print_dog - prints the struct dog
+* @d: name of the structer
+*/
+void print_dog(struct dog *d)
+{
+if (d)
+{
+d.mame ? printf("Name:%s\n", d.name) : printf("nil\n");
+d.age ? printf("Name:%f\n", d.age) : printf("nil\n");
+d.owner ? printf("Name:%s\n", d.owner) : printf("nil\n");
+}
+}
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -16,5 +16,5 @@ char *owner;
 /* Function prototype */
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
 #endif
