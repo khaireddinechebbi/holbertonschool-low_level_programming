ab649c1136922339994f0334674c1c6f283c4501
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-16 21:30:40 +0100

    4 new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,38 @@
 #include "dog.h"
 #include <stdlib.h>
 /**
+* *_strcpy - copy string
+* @dest: pointer
+* @src: pointer
+* Return: NULL
+*/
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+while ((dest[i] = src[i]) != '\0')
+{
+i++;
+}
+return (dest);
+}
+
+/**
+* _strlen - prints lenght of a string
+* @s: string
+* Return: l
+*/
+int _strlen(char *s)
+{
+int l = 0;
+while (*s != '\0')
+{
+l++;
+s++;
+}
+return (l);
+}
+
+/**
   * new_dog - creates a new struct of type dog
   * @name: struct parameter name
   * @age: struct parameter age
@@ -9,39 +41,32 @@
   */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-int n = 0, o = 0, i;
-dog_t *new_dog = malloc(sizeof(dog_t));
-
+int n = 0, o = 0;
+dog_t *new_dog;
+if (name != NULL && owner != NULL)
+{
+n = _strlen(name) + 1;
+o = _strlen(owner) + 1;
+new_dog = malloc(sizeof(dog_t));
 if (new_dog == NULL)
 return (NULL);
-
-while (name[n])
-n++;
-
-while (owner[o])
-o++;
-
-new_dog->name = malloc(sizeof(char) * (n + 1));
+new_dog->name = malloc(sizeof(char) * n);
+if (new_dog->name == NULL)
+{
+free(new_dog);
+return(NULL);
+}
 new_dog->owner = malloc(sizeof(char) * (o + 1));
-if (new_dog->owner == NULL || new_dog->name == NULL)
+if (new_dog->owner == NULL)
+{
 {
 free(new_dog->name);
-free(new_dog->owner);
 free(new_dog);
-return (NULL);
+return(NULL);
 }
-for (i = 0; i < n; i++)
-{
-new_dog->name[i] = name[i];
-}
-for (i = 0; i < o; i++)
-{
-new_dog->owner[i] = owner[i];
-}
+new_dog->name = _strcpy(new_dog->name, name);
+new_dog->owner = _strcpy(new_dog->owner, owner);
 new_dog->age = age;
-
+}
 return (new_dog);
-free(new_dog->name);
-free(new_dog->owner);
-free(new_dog);
 }
