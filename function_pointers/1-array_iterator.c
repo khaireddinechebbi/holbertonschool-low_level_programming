7333634c6357b4c07d8bd056299ddcb7971795ff
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-18 13:49:30 +0100

    1 array iterator

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+
 /**
 * array_iterator - executes a function given as a parameter on each element of an array
 * @array: array
@@ -10,7 +11,7 @@
 */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-int i;
+size_t i;
 for (i = 0; i < size; i++)
 {
 action(array[i]);
