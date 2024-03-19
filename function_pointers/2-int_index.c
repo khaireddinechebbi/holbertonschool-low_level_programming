d8a63a9b73844f6a72d07e9dfcada94aa40fefc1
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-19 09:36:01 +0100

    2 int index

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -11,14 +11,14 @@
 int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
+if (array == NULL || cmp == NULL)
+return (-1);
 if (size > 0)
 {
 for (i = 0; i < size; i++)
 {
 if (cmp(array[i]))
 return (i);
-if (array[i] == NULL || cmp == NULL)
-return (-1);
 }
 }
 return (-1);
