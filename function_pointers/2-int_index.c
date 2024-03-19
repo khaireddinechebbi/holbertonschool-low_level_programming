77370a824b733b00f82e78e522c38837e587257d
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-19 09:24:11 +0100

    2 int index

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+* int_index - searches for an integer
+* @array: array
+* @size: int
+* @cmp: function
+* Return: index or (-1)
+*/
+int int_index(int *array, int size, int (*cmp)(int))
+{
+int i;
+if (size > 0)
+{
+for (i = 0; i < size; i++)
+{
+if (cmp(array[i]))
+return (array[i]);
+}
+}
+return (-1);
+}
