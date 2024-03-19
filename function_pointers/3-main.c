4ea15dcef0b345105b87dd37707b0493776d6fd6
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-19 10:45:50 +0100

    task 3

---
+++ function_pointers/3-main.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - calculate numbers
+* @argc: argument count
+* @argv: argument vector
+* Return: 99, 100, result
+*/
+int main(int argc, char *argv[])
+{
+if (argc != 4) {
+printf("Error\n");
+return (98);
+}
+
+int num1 = atoi(argv[1]);
+int num2 = atoi(argv[3]);
+char *operator = argv[2];
+
+if (get_op_func(operator) == NULL || argv[2][1] != '\0') {
+printf("Error\n");
+return (99);
+}
+if ((*operator == '/' || *operator == '%') && num2 == 0) {
+printf("Error\n");
+return (100);
+}
+printf("%d\n", get_op_func(operator)(num1, num2));
+return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,57 @@
+#include "calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* op_add - addition
+* @a: int
+* @b: int
+* Return: result
+*/
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+* op_sub - difference
+* @a: int
+* @b: int
+* Return: result
+*/
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+* op_mul - product
+* @a: int
+* @b: int
+* Return: result
+*/
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+* op_div - division
+* @a: int
+* @b: int
+* Return: result
+*/
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+* op_mod - remainder of division
+* @a: int
+* @b: int
+* Return: result
+*/
+int op_mod(int a, int b)
+{
+return (a % b);
+}
