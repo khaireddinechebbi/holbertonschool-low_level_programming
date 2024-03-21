50ba3e401bd1795e560f6c742402b9dffd857fa8
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-21 13:01:21 +0100

    0 sum

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -3,6 +3,9 @@
 #include <stdlib.h>
 #include <stdarg.h>
 /**
+* sum_them_all - sum of all args
+* @n: number of arguments
+* Return: result
 */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -14,7 +17,7 @@ if (n == 0)
 return (0);
 
 va_start(args, n);
-for (i = 0; i > 0; )
+for (i = 0; i > 0; i++)
 sum += va_arg(args, int);
 va_end(args);
 return (sum);
