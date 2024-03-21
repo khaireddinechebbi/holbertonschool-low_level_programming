374fdc3ccf51d4eeedc674ccd3b23ab847126e43
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-21 10:48:12 +0100

    task 3

--- function_pointers/3-calc.h
+++ function_pointers/3-calc.h
@@ -1,16 +1,5 @@
-#ifndef CALC_H
-#define CALC_H
-/**
- * struct op - Struct op
- *
- * @op: The operator
- * @f: The function associated
- */
-typedef struct op
-{
-    char *op;
-    int (*f)(int a, int b);
-} op_t;
+#ifndef CAL
+#define CAL
 
 int op_add(int a, int b);
 int op_sub(int a, int b);
@@ -19,4 +8,20 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
 
+
 #endif
+
+
+
+/**
+ * struct op - Struct op
+ *
+ * @op: The operator
+ * @f: The function associated
+ */
+
+typedef struct op
+{
+    char *op;
+    int (*f)(int a, int b);
+} op_t;
