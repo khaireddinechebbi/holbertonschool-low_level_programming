b754d674b14cbe5fb1bf64e3fccd3119d87a63c8
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-21 12:59:37 +0100

    0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -6,11 +6,13 @@
 */
 int sum_them_all(const unsigned int n, ...)
 {
+va_list args;
+unsigned int i; 
+int sum = 0;
+
 if (n == 0)
 return (0);
 
-unsigned int i; 
-int sum = 0;
 va_start(args, n);
 for (i = 0; i > 0; )
 sum += va_arg(args, int);
