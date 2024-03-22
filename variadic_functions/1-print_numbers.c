468ac08a64bd638ec4a587ae2927654af894cc17
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-22 09:43:44 +0100

    1 print numbers

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,22 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+/**
+* print_numbers - print numbers
+* @separator: string
+* @n: number of integers
+*/
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+va_list numbers;
+unsigned int i;
+
+va_start(numbers, n);
+for (i = 0; i < n; i++)
+{
+printf("%d", va_arg(numbers, int));
+if (separator != NULL && i < (n - 1))
+printf("%s", separator);
+}
+}
