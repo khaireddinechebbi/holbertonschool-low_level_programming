baacd0e80d414f11316502382d8aba14c5e1b241
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-24 01:36:19 +0100

    3 print all

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -6,7 +6,7 @@
  */
 void print_char(va_list ap)
 {
-printf("%c", va_arg(ap, int));
+	printf("%c", va_arg(ap, int));
 }
 
 /**
@@ -15,7 +15,7 @@ printf("%c", va_arg(ap, int));
  */
 void print_integer(va_list ap)
 {
-printf("%d", va_arg(ap, int));
+	printf("%d", va_arg(ap, int));
 }
 
 /**
@@ -24,7 +24,7 @@ printf("%d", va_arg(ap, int));
  */
 void print_float(va_list ap)
 {
-printf("%f", va_arg(ap, double));
+	printf("%f", va_arg(ap, double));
 }
 
 /**
@@ -33,14 +33,14 @@ printf("%f", va_arg(ap, double));
  */
 void print_string(va_list ap)
 {
-char *s = va_arg(ap, char *);
+	char *s = va_arg(ap, char *);
 
-if (!s)
-{
-printf("(nil)");
-return;
-}
-printf("%s", s);
+	if (!s)
+	{
+		printf("(nil)");
+		return;
+	}
+		printf("%s", s);
 }
 
 /**
@@ -49,33 +49,34 @@ printf("%s", s);
  */
 void print_all(const char * const format, ...)
 {
-print_type types[] = {
-{"c", print_char},
-{"i", print_integer},
-{"f", print_float},
-{"s", print_string},
-{NULL, NULL}
-};
-va_list ap;
-char *separator = "";
-int i = 0;
-int j = 0;
-va_start(ap, format);
-while (format && format[i])
-{
-while (types[j].type)
-{
-if (*types[j].type == format[i])
-{
-printf("%s", separator);
-types[j].f(ap);
-separator = ", ";
-}
-++j;
-}
-j = 0;
-++i;
-}
-printf("\n");
-va_end(ap);
+	print_type types[] = {
+		{"c", print_char},
+		{"i", print_integer},
+		{"f", print_float},
+		{"s", print_string},
+		{NULL, NULL}
+	};
+	va_list ap;
+	char *separator = "";
+	int i = 0;
+	int j = 0;
+
+	va_start(ap, format);
+	while (format && format[i])
+	{
+		while (types[j].type)
+		{
+			if (*types[j].type == format[i])
+			{
+				printf("%s", separator);
+				types[j].f(ap);
+				separator = ", ";
+			}
+			++j;
+		}
+		j = 0;
+		++i;
+	}
+	printf("\n");
+	va_end(ap);
 }
