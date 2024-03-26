63f6221868510da4f5d4b885262e17465a209d78
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-26 13:17:50 +0100

    task 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -3,7 +3,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdarg.h>
-
+/**
+ * struct print - Struct print
+ *
+ * @op: The operator
+ * @f: The function print
+ */
+typedef struct print
+{
+	char *type;
+	void (*f)(va_list ap);
+} print_type;
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
