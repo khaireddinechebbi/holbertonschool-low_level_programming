1cf48cccc9fb03a2e6d8e8f27813ccfa16ea4f1e
khaireddine.chebbi <khaireddine.chebbi@live.fr>
2024-03-26 13:13:21 +0100

    task 3

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -5,5 +5,10 @@ int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+int _putchar(char c);
+void print_string(va_list ap);
+void print_char(va_list ap);
+void print_integer(va_list ap);
+void print_float(va_list ap);
 
 #endif
