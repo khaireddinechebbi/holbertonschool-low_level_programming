3d86ca1ee00b1735581ddc6cebb5f1fca1262790
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-04-04 10:36:39 +0100

    2 add nodes

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -1,7 +1,8 @@
 #include "lists.h"
 /**
 * *add_node - adds a new node at the beginning of a list_t list
-* @str: string to list
+* @str: pointer to char
+* @head: pointer to a pointer
 * Return:  the address of the new element
 */
 list_t *add_node(list_t **head, const char *str)
@@ -17,7 +18,7 @@ for (i = 0; str[i]; i++)
 
 node = malloc(sizeof(list_t));
 if (node == NULL)
-return(NULL);
+return (NULL);
 
 node->str = strdup(str);
 if (node->str == NULL)
