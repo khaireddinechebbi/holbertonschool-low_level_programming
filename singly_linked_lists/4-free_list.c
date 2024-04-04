782bcedd245aad6a12e903326e9abfea846038c1
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-04-04 14:14:49 +0100

    4 free list

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -1,7 +1,17 @@
 #include "lists.h"
 /**
+* free_list - frees lists
+* @head: pointer to a list
 */
 void free_list(list_t *head)
 {
-free(head);
+list_t *t;
+
+while (head != NULL)
+{
+t = head;
+head = head->next;
+free(t->str);
+free(t);
+}
 }
