580bc91fa136bcd7f1ede6b3948adf827b85c3f7
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-04-04 10:04:44 +0100

    1 list len

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,5 +1,8 @@
 #include "lists.h"
 /**
+* list_len - returns the number of elements in a linked list_t list
+* @h: pointer of the list
+* Return: number of elements
 */
 size_t list_len(const list_t *h)
 {
