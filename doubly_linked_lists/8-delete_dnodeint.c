3a8667fa563ecaa6f65bff16223f67011b506e95
khaireddinechebbi <khaireddine.chebbi@live.fr>
2024-04-05 10:14:54 +0100

    8 delete

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -6,7 +6,7 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 dlistint_t *d, *t = *head;
 unsigned int i;
 
-if (head == NULL)
+if (*head == NULL)
 return (-1);
 
 if (index == 0)
